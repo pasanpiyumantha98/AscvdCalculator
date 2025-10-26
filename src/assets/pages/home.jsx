b43772ad19a630b2297390bf6a8bd622c9a52098
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-10-26 19:23:39 +0530

    n1

--- src/assets/pages/home.jsx
+++ src/assets/pages/home.jsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from "react";
-import Footer from "../../components/footer";
-import Header from "../../components/header";
+import Footer from "../components/footer";
+import Header from "../components/header";
 
 
 /**
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
+import React from "react";                           // 👈 React must be imported first
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import "./index.css";
+import Home from "./assets/pages/home.jsx";
+import Privacy from "./assets/pages/privacy.jsx";
+import Contact from "./assets/pages/contact.jsx";
 
-import {BrowserRouter,Route,Routes} from "react-router-dom";
-import Home from './assets/pages/index/home.jsx';
-import Privacy from './assets/pages/privacy.jsx';
-import Contact from './assets/pages/contact.jsx';
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <Routes>
-  <Route path="/" element={<Home />}/>
-  <Route path="/privacy-policy" element={<Privacy />}/>
-  <Route path="/contact-us" element={<Contact />}/>
-
-  </Routes>
-    
-  </BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/privacy-policy" element={<Privacy />} />
+        <Route path="/contact-us" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
